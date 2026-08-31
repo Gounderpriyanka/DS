44bc9da902ed8a4bc9b7bab33d241e3cfa848f8e
Gounderpriyanka <gounderpriyanka4@gmail.com>
2026-09-01 01:15:41 +0530

    ,,,,

--- Statistics/Session-4.py
+++ Statistics/Session-4.py
@@ -28,32 +28,50 @@ creating a long right tail.'''
 #  [5, 7, 8, 8, 9, 10, 12, 15, 95]. Calculate the mean and median, then state which is
 #  higher and what that tells you about the skewness of the data.If the mean is pulled away from
 #  the median, the data is skewed in that direction.
-
-import pandas as pd
+'''import numpy as np
 data = [5, 7, 8, 8, 9, 10, 12, 15, 95]
 
-df = pd.DataFrame({
-    "instagram_user" : range(1,10),
-    "data" : [5, 7, 8, 8, 9, 10, 12, 15, 95]
-})
-print(df)
-mean = df["data"].mean()
-median = df["data"].median()
+mean = np.mean(data)
+median = np.median(data)
 
-print("Mean:",mean)
-print("Median:",median)
+print("Mean:",mean) # 18.777
+print("Median:",median) # 9.0
 
 if mean>median:
     print("Mean is higher than median.")
     print("The data is right-skewed")
-
-
+'''
 
 
 # 3.Find a real-world example of right-skewed data from any app you use (like Zomato order amounts, Paytm wallet balances,
 #  or YouTube video views). Briefly describe the dataset and explain why you think it is right skewed.
+'''import numpy as np
+youtube_views = [100, 150, 200, 250, 300, 500, 800, 1500, 10000]
 
+
+mean = np.mean(youtube_views)
+median = np.median(youtube_views)
+print(mean) # 1533.33
+print(median) # 300.0'''
+
+'''Mean > Median
+Therefore, the data is right-skewed (positively skewed).'''
 
 
 # 4.Given this dataset of Flipkart product ratings: [3, 3, 4, 4, 4, 5, 5, 5, 5, 5], draw a rough graph and 
 # state if the distribution is symmetrical, left skewed, or right skewed. Justify your answer in one line.
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+ratings = [3, 3, 4, 4, 4, 5, 5, 5, 5, 5]
+
+values, counts = np.unique(ratings, return_counts=True)
+
+plt.bar(values, counts)
+
+plt.xlabel("Flipkart Product Rating")
+plt.ylabel("Frequency")
+plt.title("Flipkart Product Ratings")
+
+plt.show()
