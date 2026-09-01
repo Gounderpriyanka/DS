2140bddbcd1ec34c5769331da5fab1f76aec1bad
Gounderpriyanka <gounderpriyanka4@gmail.com>
2026-09-01 14:54:02 +0530

    ......

--- Statistics/Session-2.py
+++ Statistics/Session-2.py
@@ -1,26 +1,21 @@
 #1.Create a Python script called playlist_stats.py that takes a list of daily Spotify song
 #  play counts (e.g., [120, 135, 150, 200, 120, 90, 200]) and calculates the mean (average) 
 # number of plays.
-
-'''import  pandas as pd
-
+'''import numpy as np
 playlist_stats = [120, 135, 150, 200, 120, 90, 200]
-result = pd.Series(playlist_stats)
-print(result.mean())
-'''
+print(np.mean(playlist_stats))'''
+
 # 2.Given the following array of delivery times (in minutes) for Zomato orders: 
 # [30, 25, 40, 35, 30, 45, 30], write code to find the median delivery time and print it.
 
-'''import pandas as pd
+'''import numpy as np
 l1 = [30, 25, 40, 35, 30, 45, 30]
-
-Result = pd.Series(l1)
-print(Result.median())'''
-
+print(np.median(l1))
+'''
 #3.Write a function most_common_rating(ratings) that takes a list of Flipkart product ratings 
 # (e.g., [5, 4, 4, 3, 5, 4, 2, 4]) and returns the mode (most frequent rating)
 
-import pandas as pd
+'''import pandas as pd
 
 def most_common_rating(ratings):
     result = pd.Series(ratings).mode()
@@ -28,7 +23,7 @@ def most_common_rating(ratings):
 
 l1 = [5, 4, 4, 3, 5, 4, 2, 4]
 
-print("Most Common Rating:", most_common_rating(l1))
+print("Most Common Rating:", most_common_rating(l1))'''
 
 #4.Given three lists representing YouTube video views for three different channels, 
 # compare the mean, median, and mode for each channel, and decide which channel's data is 
---
+++ Statistics/Session-6.py
@@ -0,0 +1,26 @@
+# 1.Write down the null hypothesis (H0) and alternative hypothesis (H1) for the following scenario: 
+# You believe that people spend more time on Instagram than on YouTube daily.
+
+'''1. Null Hypothesis and Alternative Hypothesis
+
+Null Hypothesis (H₀): People do not spend more time on Instagram than on YouTube daily.
+Alternative Hypothesis (H₁): People spend more time on Instagram than on YouTube daily.'''
+
+# 2.Given the data: Group A (users shown a new Zomato home page) had an average order value of 
+# ₹350 from 30 users, and Group B (old home page) had an average order value of ₹320 from 30 users.
+# Assume both groups have a standard deviation of ₹50. Calculate the p-value using a two-sample t-test
+# (you may use Python, R, or an online calculator), and decide whether to accept or reject the null hypothesis
+# at a 0.05 significance level.Use scipy.stats.ttest_ind in Python or 
+# any online t-test calculator if you are not comfortable with code.
+
+
+
+# 3.Pick any feature from your favorite app (like the new dark mode in WhatsApp or a redesigned payment button in
+#  Paytm) and describe how you would set up an A/B test for it. Clearly state what your control and variant
+# groups would be, what metric you would measure, and what your null and alternative hypotheses are.
+
+
+# 4.Suppose you run an A/B test for a new 'Add to Wishlist' button design on Flipkart. Out of 1000 users, 
+# 520 clicked the new button versus 480 who clicked the old button. Without doing detailed calculations, 
+# explain if this difference is likely to be significant or just due to chance. Briefly explain how the p-value
+# helps you decide.
