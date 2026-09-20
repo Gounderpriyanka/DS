97144f98fd4491a3ce396ba2d3e2031c8f810873
Gounderpriyanka <gounderpriyanka4@gmail.com>
2026-09-20 19:32:29 +0530

    .......

--- Working with Databases/Session-02.sql
+++ Working with Databases/Session-02.sql
@@ -16,23 +16,24 @@ create table restaurants(
 create table users(
 	user_id int primary key,
     username varchar(100),
-    email varchar(50),
-    phone_number int
+    email varchar(50) unique,
+    phone_number varchar(15) unique
     created_at datetime 
-)
+);
 
 -- Task 5
 -- Intentionally created an error first
 -- Error: missing comma after username column
 
 -- Corrected statement
-CREATE TABLE users (
-    user_id INT PRIMARY KEY,
-    username VARCHAR(100),
-    email VARCHAR(100) UNIQUE,
-    phone_number VARCHAR(15) UNIQUE,
-    created_at DATETIME
-);
+create table users (
+    user_id int primary key,
+    username varchar(100),
+    email varchar(100) unique,
+    phone_number varchar(15) unique,
+    created_at datetime
+)
+
 
 
 
