9f8fde804a26d7f45d707a16872582161809f13d
Gounderpriyanka <gounderpriyanka4@gmail.com>
2026-09-21 16:35:13 +0530

    ...

--- Working with Databases/Session-03.sql
+++ Working with Databases/Session-03.sql
@@ -1,11 +1,37 @@
 use music_streaming_app;
 
+select * from playlist;
 -- Task 1
 create table Playlist(
 	id int primary key,
     song_name varchar(100),
     artist varchar(100),
-    duartion int 
+    duration int 
 );
 
-insert into playlist value(1,"Unstoppable","Sia",217)
+insert into playlist value(1,"Unstoppable","Sia",217);
+
+-- Task 2
+insert into playlist values
+(2,"Believer","Imagine Dragons",204),
+(3,"Hall of Fame","The Script",203),
+(4,"The Climb","Miley Cyrus",216);
+
+-- Task 3
+update playlist
+set song_name = "Hall"
+where  id = 3;
+
+-- Task 4
+
+SET SQL_SAFE_UPDATES = 0;
+delete from playlist where duration<120;
+SET SQL_SAFE_UPDATES = 1;
+
+-- Task 5 
+SET SQL_SAFE_UPDATES = 0;
+update playlist
+set song_name = concat(song_name,"Remix")
+where artist = "The Script" and duration > 120;
+SET SQL_SAFE_UPDATES = 1;
+
