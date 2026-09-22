b085ad0b46fdeb99c858c7a53e021cc735aaa949
Gounderpriyanka <gounderpriyanka4@gmail.com>
2026-09-22 13:11:24 +0530

    ....

--- Working with Databases/Session-04.sql
+++ Working with Databases/Session-04.sql
@@ -21,3 +21,31 @@ Select * from MusicPlaylist;
 
 -- Task 2
 
+-- Task 3
+use foodie_app;
+
+create table FoodOrders(
+	id int primary key,
+    restaurant varchar(100),
+    food_item varchar(100),
+    order_date date
+);
+
+insert into FoodOrders value
+(1,"Domino's","Pizza","2026-09-10");
+
+insert into FoodOrders values
+(2,"McDonald's","Burger","2026-09-11"),
+(3,"Domino's","Pasta","2026-09-12"),
+(4,"Subway","Sandwich","2026-09-13"),
+(5,"McDonald's","Fries","2026-09-14"),
+(6,"Domino's","Garlic Bread","2026-09-15"),
+(7,"subway","Wrap","2026-09-16");
+
+select * from Foodorders;
+
+-- Task 4
+Select food_item as Dish,order_date as Date_Ordered
+from foodorders
+
+-- Task 5
